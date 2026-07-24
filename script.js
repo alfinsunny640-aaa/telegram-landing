39e0aa82a7b346a673b73fbf5a3b7cef4d54ce81
alfinsunny <alfinsunny640@gmail.com>
2026-07-24 11:01:45 +0530

    Add Lead and Subscribe event tracking for Meta Pixel

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ const button = document.querySelector(".btn");
 button.addEventListener("click", () => {
 
     console.log("Telegram Clicked");
+    if (typeof fbq === 'function') {
+        fbq('track', 'Lead');
+        fbq('track', 'Subscribe');
+    }
 
 });
